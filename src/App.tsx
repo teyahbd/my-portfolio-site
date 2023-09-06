4356e1f032ca76b2843cae0006fcce91730a3a83
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-09-06 16:50:12 +0100

    feat: add components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import "./App.css";
 import Header from "./organisms/Header/Header";
+import Contact from "./organisms/Contact/Contact";
 import Home from "./pages/Home/Home";
+import Projects from "./pages/Projects/Projects";
+import CreditFooter from "./molecules/CreditFooter/CreditFooter";
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <Projects />
+      <footer>
+        <Contact />
+        <CreditFooter />
+      </footer>
     </>
   );
 }
