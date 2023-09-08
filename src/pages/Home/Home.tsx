292ca23ae2cc3300d3b21474906e34ac53f60061
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-09-08 15:47:48 +0100

    feat: make home page more responsive

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,12 +2,11 @@ import "./styles.css";
 
 function Home() {
   return (
-    <section>
+    <section id="home">
       <p id="intro-text">
         Hi, I'm <span className="hover-color">Teyah</span>! I'm a
         <span className="hover-color"> software engineer</span> based in
-        <span className="hover-color"> Manchester</span>. Maybe a little more
-        detail here and redesign.
+        <span className="hover-color"> Manchester</span>.
       </p>
       <div
         style={{
