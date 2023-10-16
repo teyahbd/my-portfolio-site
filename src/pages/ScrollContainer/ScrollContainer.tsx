356e89de863b9ac4d267df036dec537b7c42ccbe
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-10-16 18:17:27 +0100

    refactor: simplify project display

--- src/pages/ScrollContainer/ScrollContainer.tsx
+++ src/pages/ScrollContainer/ScrollContainer.tsx
@@ -1,226 +1,55 @@
 import CreditFooter from "../../atoms/CreditFooter/CreditFooter";
 import ContactFooter from "../../organisms/ContactFooter/ContactFooter";
-import Project from "../../molecules/Project/Project";
+import Project from "../../organisms/Project/Project";
 import "./styles.css";
-import { Info } from "../../App";
+import { Info, ProjectFields } from "../../App";
 import IntroParagraph from "../../atoms/IntroParagraph/IntroParagraph";
+import LinkButton from "../../atoms/LinkButton/LinkButton";
 
-interface ScrollContainer {
+interface ScrollContainerProps {
   intro: string;
-  projects: object;
+  projects: ProjectFields[];
   info: Info;
 }
 
-function ScrollContainer(props: ScrollContainer) {
+function ScrollContainer(props: ScrollContainerProps) {
   return (
     <div id="scroll-container" className="fade-in-slow">
-      <IntroParagraph text={props.intro} />
-      {props.projects["2023"].length > 0 ? (
-        <h4 className="year">- 2023 -</h4>
-      ) : (
-        <></>
-      )}
-      {props.projects["2023"].map((project) => {
-        const hasGithubLink = project.fields.repo_link;
-        const hasHostedLink = project.fields.hosted_link;
+      <IntroParagraph text={props.intro} forMobile={false} />
+      {/* <div id="ombre-container">
+        <div id="mini-ombre-circle" className="fade-in-quick"></div>
+        <div id="mini-ombre-circle" className="fade-in-quick"></div>
+        <div id="mini-ombre-circle" className="fade-in-quick"></div>
+      </div> */}
+      {props.projects.map((project, index) => {
         return (
-          <Project>
-            <div className="project-header">
-              <h4 className="project-name">
-                {project.fields.name ? project.fields.name.toLowerCase() : ""}
-              </h4>
-              <div className="project-icon-container">
-                {hasGithubLink ? (
-                  <a
-                    className="project-link-icon"
-                    href={project.fields.repo_link}
-                  >
-                    <i className="fa-solid fa-globe"></i>
-                  </a>
-                ) : (
-                  <></>
-                )}
-                {hasHostedLink ? (
-                  <a
-                    className="project-link-icon"
-                    href={project.fields.hosted_link}
-                  >
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                ) : (
-                  <></>
-                )}
-              </div>
-            </div>
-            <p className="project-desc">{project.fields.description}</p>
-          </Project>
-        );
-      })}
-      {props.projects["2022"].length > 0 ? (
-        <h4 className="year">- 2022 -</h4>
-      ) : (
-        <></>
-      )}
-      {props.projects["2022"].map((project) => {
-        const hasGithubLink = project.fields.repo_link;
-        const hasHostedLink = project.fields.hosted_link;
-        return (
-          <Project>
-            <div className="project-header">
-              <h4 className="project-name">
-                {project.fields.name ? project.fields.name.toLowerCase() : ""}
-              </h4>
-              <div className="project-icon-container">
-                {hasGithubLink ? (
-                  <a
-                    className="project-link-icon"
-                    href={project.fields.repo_link}
-                  >
-                    <i className="fa-solid fa-globe"></i>
-                  </a>
-                ) : (
-                  <></>
-                )}
-                {hasHostedLink ? (
-                  <a
-                    className="project-link-icon"
-                    href={project.fields.hosted_link}
-                  >
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                ) : (
-                  <></>
-                )}
-              </div>
-            </div>
-            <p className="project-desc">{project.fields.description}</p>
-          </Project>
-        );
-      })}
-      {props.projects["2021"].length > 0 ? (
-        <h4 className="year">- 2021 -</h4>
-      ) : (
-        <></>
-      )}
-      {props.projects["2021"].map((project) => {
-        const hasGithubLink = project.fields.repo_link;
-        const hasHostedLink = project.fields.hosted_link;
-        return (
-          <Project>
-            <div className="project-header">
-              <h4 className="project-name">
-                {project.fields.name ? project.fields.name.toLowerCase() : ""}
-              </h4>
-              <div className="project-icon-container">
-                {hasGithubLink ? (
-                  <a
-                    className="project-link-icon"
-                    href={project.fields.repo_link}
-                  >
-                    <i className="fa-solid fa-globe"></i>
-                  </a>
-                ) : (
-                  <></>
-                )}
-                {hasHostedLink ? (
-                  <a
-                    className="project-link-icon"
-                    href={project.fields.hosted_link}
-                  >
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                ) : (
-                  <></>
-                )}
-              </div>
-            </div>
-            <p className="project-desc">{project.fields.description}</p>
-          </Project>
-        );
-      })}
-      {props.projects["2020"].length > 0 ? (
-        <h4 className="year">- 2020 -</h4>
-      ) : (
-        <></>
-      )}
-      {props.projects["2020"].map((project) => {
-        const hasGithubLink = project.fields.repo_link;
-        const hasHostedLink = project.fields.hosted_link;
-        return (
-          <Project>
-            <div className="project-header">
-              <h4 className="project-name">
-                {project.fields.name ? project.fields.name.toLowerCase() : ""}
-              </h4>
-              <div className="project-icon-container">
-                {hasGithubLink ? (
-                  <a
-                    className="project-link-icon"
-                    href={project.fields.repo_link}
-                  >
-                    <i className="fa-solid fa-globe"></i>
-                  </a>
-                ) : (
-                  <></>
-                )}
-                {hasHostedLink ? (
-                  <a
-                    className="project-link-icon"
-                    href={project.fields.hosted_link}
-                  >
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                ) : (
-                  <></>
-                )}
-              </div>
-            </div>
-            <p className="project-desc">{project.fields.description}</p>
-          </Project>
-        );
-      })}
-      {props.projects["2019"].length > 0 ? (
-        <h4 className="year">- 2019 -</h4>
-      ) : (
-        <></>
-      )}
+          <>
+            <Project>
+              {index !== 0 ? <p className="separator">✿✿✿</p> : <></>}
+              <div className="project-header">
+                <h4 className="project-name">
+                  {(project.name ?? "").toLowerCase()}
+                </h4>
+                <div className="project-links">
+                  {project.repo_link ? (
+                    <LinkButton name="github" link={project.repo_link} />
+                  ) : (
+                    <></>
+                  )}
 
-      {props.projects["2019"].map((project) => {
-        const hasGithubLink = project.fields.repo_link;
-        const hasHostedLink = project.fields.hosted_link;
-        return (
-          <Project>
-            <div className="project-header">
-              <h4 className="project-name">
-                {project.fields.name ? project.fields.name.toLowerCase() : ""}
-              </h4>
-              <div className="project-icon-container">
-                {hasGithubLink ? (
-                  <a
-                    className="project-link-icon"
-                    href={project.fields.repo_link}
-                  >
-                    <i className="fa-solid fa-globe"></i>
-                  </a>
-                ) : (
-                  <></>
-                )}
-                {hasHostedLink ? (
-                  <a
-                    className="project-link-icon"
-                    href={project.fields.hosted_link}
-                  >
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                ) : (
-                  <></>
-                )}
+                  {project.hosted_link ? (
+                    <LinkButton name="globe" link={project.hosted_link} />
+                  ) : (
+                    <></>
+                  )}
+                </div>
               </div>
-            </div>
-            <p className="project-desc">{project.fields.description}</p>
-          </Project>
+              <p className="project-desc">{project.description ?? ""}</p>
+            </Project>
+          </>
         );
       })}
+
       <ContactFooter
         email={props.info.email ?? ""}
         info={props.info}
