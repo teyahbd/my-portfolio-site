7c2943db5d5079a06c6643c2dce87b43e0dbcd79
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-11-28 12:28:28 +0000

    feat: add index to project

--- src/organisms/Project/Project.tsx
+++ src/organisms/Project/Project.tsx
@@ -2,11 +2,16 @@ import { ReactNode } from "react";
 import "./styles.css";
 
 interface ProjectProps {
+  index: number;
   children: ReactNode;
 }
 
-function Project({ children }: ProjectProps) {
-  return <article className="project">{children}</article>;
+function Project({ children, index }: ProjectProps) {
+  return (
+    <article className="project" key={index}>
+      {children}
+    </article>
+  );
 }
 
 export default Project;
