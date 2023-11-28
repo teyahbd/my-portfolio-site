0566b4e66e0840a80bd2c28fab1db712017009e8
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-11-28 12:27:57 +0000

    refactor: update intro styling

--- src/atoms/IntroParagraph/IntroParagraph.tsx
+++ src/atoms/IntroParagraph/IntroParagraph.tsx
@@ -11,12 +11,13 @@ function IntroParagraph(props: IntroParagraphProps) {
       id="intro"
       className={`${props.forMobile ? "mobile-only" : "web-only"}`}
     >
-      {props.text.map((paragraph) => {
+      {props.text.map((paragraph, index) => {
         return (
           <p
             className={`intro-paragraph ${
               props.forMobile ? "mobile-only fade-in-slow" : "web-only"
             }`}
+            key={index}
           >
             {paragraph}
           </p>
