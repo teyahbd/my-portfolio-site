04d3bb7c3725832b39db79dceb4ceae456646bf7
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-11-29 16:05:39 +0000

    refactor: improve intro styling and language

--- src/atoms/IntroParagraph/IntroParagraph.tsx
+++ src/atoms/IntroParagraph/IntroParagraph.tsx
@@ -11,12 +11,12 @@ function IntroParagraph(props: IntroParagraphProps) {
       id="intro"
       className={`${props.forMobile ? "mobile-only" : "web-only"}`}
     >
-      {props.text.map((paragraph) => {
+      {props.text.map((paragraph, index) => {
         return (
           <p
             className={`intro-paragraph ${
               props.forMobile ? "mobile-only fade-in-slow" : "web-only"
-            }`}
+            } ${index === 0 ? "top-paragraph" : null}`}
           >
             {paragraph}
           </p>
--- src/data/intro.ts
+++ src/data/intro.ts
@@ -2,7 +2,7 @@ export const getIntro = (): string[] => {
   const intro = [
     "Hello!",
     "I'm a software engineer based in lovely Manchester 🐝  I currently work within the internal development team at Northcoders.",
-    "I'm proud to be a versatile developer - you can find me crafting React UIs one day and deploying AWS event infrastructure the next. Any problems that can be solved with code are my thing! Bonus points for cloud, DevOps and collaborating with smart, light-hearted people ✨",
+    "I'm proud to be a versatile developer - you can find me crafting React UIs one day and deploying AWS event infrastructure the next. Any problems that can be solved with code are my thing! Bonus points for cloud, DevOps and collaborating with great people ✨",
   ];
   return intro;
 };
